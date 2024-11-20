a2b5265854a47b615d6e6700ab77de395159eb41
Llorens19 <llorenssorianodiego@gmail.com>
2024-11-20 13:07:12 +0100

    Register funcionando

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,25 +3,42 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Admin;
 use App\Http\Requests\User\RegisterUserRequest;
+use App\Http\dto\UserDTO;
 
 class UserController extends Controller
 {
-
-
     public function register(RegisterUserRequest $request)
     {
         try {
             $validated = $request->validated();
-            $hasshedPassword = bcrypt($validated['password']);
+
+            if (User::where('email', $validated['email'])->exists()) {
+                return response()->json(['message' => 'Email is already taken'], 400);
+            }
+
+            if (User::where('username', $validated['username'])->exists()) {
+                return response()->json(['message' => 'Username is already taken'], 400);
+            }
+
+            $hashedPassword = bcrypt($validated['password']);
             $user = User::create([
                 'img_user' => $validated['img_user'],
                 'email' => $validated['email'],
                 'username' => $validated['username'],
-                'password' => $hasshedPassword,
+                'password' => $hashedPassword,
                 'type_user' => 'admin',
             ]);
-            return response()->json($user, 201);
+            
+            Admin::create([
+                'id_user' => $user->id_user, 
+            ]);
+            
+            $resp = UserDTO::registerDto($user);
+            
+            return response()->json( $resp, 201);
+            
         } catch (\Exception $e) {
             return response()->json(['message' => 'Error to create user', 'error' => $e->getMessage()], 400);
         }
